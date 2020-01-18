4f53220e04c08cd84f65d68f043188ebf956dd4d
mypsd <60044527+mypsd@users.noreply.github.com>
2020-01-19 01:56:51 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,12 +21,9 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="increase-in-sophisticated-digital-crime.html"}
+if(x=="1"){xxx="5-ways-fukushima-radiation-releases-are.html"}
 if(x=="2"){xxx="data-science-and-its-rising-importance.html"}
-if(x=="3"){xxx="leukemia-treatment-breakthroughs-for.html"}
-if(x=="4"){xxx="5-ways-fukushima-radiation-releases-are.html"}
-if(x=="5"){xxx="5-benefits-of-marble-floors.html"}
-if(x=="6"){xxx="10-tips-to-help-you-repair-your-wooden.html"}a_to_ve[i].href="https://blog.sadesain.net/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="10-tips-to-help-you-repair-your-wooden.html"}a_to_ve[i].href="https://mypsd-template.blogspot.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
