ce6b9b3134c0437127e03197cd67ab6fb56e6c06
mypsd <60044527+mypsd@users.noreply.github.com>
2020-01-19 01:58:58 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,8 +22,8 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="5-ways-fukushima-radiation-releases-are.html"}
-if(x=="2"){xxx="data-science-and-its-rising-importance.html"}
-if(x=="3"){xxx="10-tips-to-help-you-repair-your-wooden.html"}a_to_ve[i].href="https://mypsd-template.blogspot.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="10-tips-to-help-you-repair-your-wooden.html"}
+if(x=="3"){xxx="data-science-and-its-rising-importance.html"}a_to_ve[i].href="https://mypsd-template.blogspot.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
